4717adf71cc40b188eeb3b7a089a1920144ea36a
Kapongoboy <plamedi.luis11@gmail.com>
2023-01-21 17:24:20 +0200

    fixed new theme not taking

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,6 @@
 function ColorMyPencils(color)
-	color = color or "catppuccin"
+	-- color = color or "catppuccin"
+	color = color or "kanagawa"
 	vim.cmd.colorscheme(color)
 
 	--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- lua/plamedi/init.lua
+++ lua/plamedi/init.lua
@@ -1,10 +1,10 @@
-local ok, catppuccin = pcall(require, "catppuccin")
-if not ok then return end
-vim.g.catppuccin_flavour = "macchiato"
-catppuccin.setup()
+-- local ok, catppuccin = pcall(require, "catppuccin")
+-- if not ok then return end
+-- vim.g.catppuccin_flavour = "macchiato"
+-- catppuccin.setup()
 -- vim.cmd[[colorscheme catppuccin]]
 --
-vim.cmd("colorscheme kanagawa")
+vim.cmd[[colorscheme kanagawa]]
 require("plamedi.remap")
 require("plamedi.packer")
 require("plamedi.set")
